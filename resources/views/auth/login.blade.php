c60e075f7dbe881d94d2aaaad2816da33f66b0dd
Cezar Rômulo Cordeiro de Araújo Filho <cezar.cordeiro@dce.ufpb.br>
2020-03-26 03:18:51 -0300

    refactor tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,18 @@
 @section('content')
 <div class="container" style="height: auto;">
   <div class="row align-items-center">
-    <div class="col-md-9 ml-auto mr-auto mb-3 text-center">
+    <!-- <div class="col-md-9 ml-auto mr-auto mb-3 text-center">
       <h3>{{ __('Log in to see how you can speed up your web development with out of the box CRUD for #User Management and more.') }} </h3>
-    </div>
-    <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+    </div> -->
+    <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto " style="margin-top: 80px;">
       <form class="form" method="POST" action="{{ route('login') }}">
         @csrf
 
         <div class="card card-login card-hidden mb-3">
           <div class="card-header card-header-primary text-center">
+            <div class="img-logo">
+              <img src="/material/img/arena-flores-logo.png" alt="image" height="70">
+            </div>
             <h4 class="card-title"><strong>{{ __('Login') }}</strong></h4>
             <!-- <div class="social-line">
               <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
@@ -26,7 +29,6 @@
             </div> -->
           </div>
           <div class="card-body">
-            <p class="card-description text-center">{{ __('Or Sign in with ') }} <strong>admin@material.com</strong> {{ __(' and the password ') }}<strong>secret</strong> </p>
             <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
               <div class="input-group">
                 <div class="input-group-prepend">
@@ -59,7 +61,7 @@
             </div>
             <div class="form-check mr-auto ml-3 mt-3">
               <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember me') }}
+                <input class="form-check-input" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Lembre-se de mim') }}
                 <span class="form-check-sign">
                   <span class="check"></span>
                 </span>
@@ -67,11 +69,11 @@
             </div>
           </div>
           <div class="card-footer justify-content-center">
-            <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Lets Go') }}</button>
+            <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('ENTRAR') }}</button>
           </div>
         </div>
       </form>
-      <div class="row">
+      <!-- <div class="row">
         <div class="col-6">
           @if (Route::has('password.request'))
           <a href="{{ route('password.request') }}" class="text-light">
@@ -84,7 +86,7 @@
             <small>{{ __('Create new account') }}</small>
           </a>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
