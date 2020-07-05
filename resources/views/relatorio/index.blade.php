85e8f9b181612e6b4b1cd47aa0bd775ea459b9b4
saucesar <sau.cesarlima2013@gmail.com>
2020-07-05 00:47:49 -0300

    Tela básica para relatorio.

---
+++ resources/views/relatorio/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app', ['activePage' => 'relatorios', 'titlePage' => __('Relatorios')])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title ">Relatorios</h4>
+                    </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="alert alert-success">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <i class="material-icons">close</i>
+                                    </button>
+                                    <span>{{ session('status') }}</span>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                        <div class="table-responsive">
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
