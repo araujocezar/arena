8e2389a9e144fb5ee5dede85cbcb7f4eab7baea9
saucesar <sau.cesarlima2013@gmail.com>
2020-07-05 00:45:37 -0300

    Busca de aluno por cpf na tela inicial

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,25 +6,47 @@ use Illuminate\Database\Eloquent\Builder;
 use App\Aluno;
 use App\Plano;
 use Illuminate\Http\Request;
-use function foo\func;
+use Illuminate\Support\Facades\DB;
 
 class AlunoController extends Controller
 {
+    public function inicio()
+    {
+        return view('aluno.inicio');
+    }
+
+    public function buscarAluno(Request $request)
+    {
+        $aluno = Aluno::firstWhere('cpf', $request->cpfAluno);
+        if ($aluno) {
+            $alunoPlano = DB::select('select * from aluno_plano where aluno_id = ?', [$aluno->id]);
+            $planos = [];
+            foreach ($alunoPlano as $plano) {
+                $planos[] = Plano::firstWhere('id', $plano->id);
+            }
+        } else {
+            return back()->with('erro', 'Aluno não Encontrado, insira um CPF valido!');
+        }
+
+        return view('aluno.inicio', ['aluno' => $aluno, 'planos' => $planos]);
+    }
+
     public function listarAlunos($parametros)
     {
         try {
             $lista = [];
-            if ($parametros == "todos") {
+            if ($parametros == 'todos') {
                 $lista = Aluno::all();
-            } else if ($parametros == "futvolei") {
+            } elseif ($parametros == 'futvolei') {
                 $lista = Aluno::whereHas('planos', function (Builder $query) {
                     $query->where('categoria_id', '=', '1');
                 })->get();
-            } else if ($parametros == "funcional") {
+            } elseif ($parametros == 'funcional') {
                 $lista = Aluno::whereHas('planos', function (Builder $query) {
                     $query->where('categoria_id', '=', '2');
                 })->get();
             }
+
             return view('aluno.listagem-alunos', ['alunos' => $lista, 'tipo' => $parametros]);
         } catch (\Exception $e) {
             dd($e);
@@ -33,10 +55,11 @@ class AlunoController extends Controller
 
     public function filtrar_aluno_cpf($tipo, Request $request)
     {
+        dd($request->all());
         $busca = trim($request->busca);
         if ($busca === null || $busca === '') {
             $aluno = Aluno::all();
-        } else if ($this->verificaCpf($busca)) { //após a verificação, valida se é um CPF, se for entra nesse primeiro if, caso contrario ele busca por nome
+        } elseif ($this->verificaCpf($busca)) { //após a verificação, valida se é um CPF, se for entra nesse primeiro if, caso contrario ele busca por nome
             $cpf = $this->limpaCPF_CNPJ($busca);
             if ($tipo != 'todos') { // caso a listagem não seja do tipo todos, filtra baseado na categoria do plano
                 $aluno = Aluno::where('cpf', '=', $cpf)->whereHas('planos', function (Builder $query) use ($tipo) {
@@ -49,22 +72,22 @@ class AlunoController extends Controller
             }
         } else {
             if ($tipo != 'todos') { // caso a lisatagem não seja do tipo todos, filtra baseado na categoria do plano
-                $aluno = Aluno::where('nome', 'ilike',  '%' . $busca . '%')
+                $aluno = Aluno::where('nome', 'ilike', '%'.$busca.'%')
                     ->whereHas('planos', function (Builder $query) use ($tipo) {
                         $query->whereHas('categorias', function (Builder $categoria) use ($tipo) {
                             $categoria->where('tipo', 'like', $tipo);
                         });
                     })->get();
             } else {
-                $aluno = Aluno::where('nome', 'ilike',  '%' . $busca . '%')->get();
+                $aluno = Aluno::where('nome', 'ilike', '%'.$busca.'%')->get();
             }
         }
+
         return view('aluno.listagem-alunos', ['alunos' => $aluno, 'tipo' => $tipo]);
     }
 
-    function verificaCpf($cpf)
+    public function verificaCpf($cpf)
     {
-
         $cpf = preg_replace('/[^0-9]/is', '', $cpf);
         if (strlen($cpf) != 11) {
             return false;
@@ -72,23 +95,25 @@ class AlunoController extends Controller
         if (preg_match('/(\d)\1{10}/', $cpf)) {
             return false;
         }
-        for ($t = 9; $t < 11; $t++) {
-            for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $cpf{
-                    $c} * (($t + 1) - $c);
+        for ($t = 9; $t < 11; ++$t) {
+            for ($d = 0, $c = 0; $c < $t; ++$c) {
+                $d += $cpf[
+                    $c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
-            if ($cpf{
-                $c} != $d) {
+            if ($cpf[
+                $c] != $d) {
                 return false;
             }
         }
+
         return true;
     }
 
-    function limpaCPF_CNPJ($valor)
+    public function limpaCPF_CNPJ($valor)
     {
         $valor = preg_replace('/[^0-9]/', '', $valor);
+
         return $valor;
     }
 
@@ -97,6 +122,7 @@ class AlunoController extends Controller
         try {
             $aluno = Aluno::find($id);
             $aluno->delete();
+
             return redirect()->route('listagem-alunos', $tipo)->withStatus(__('Aluno removido com sucesso'));
         } catch (\Exception $e) {
             return redirect()->route('listagem-alunos', $tipo)->withErros(__('Erro ao deletar aluno'));
@@ -108,17 +134,18 @@ class AlunoController extends Controller
         $planos = Plano::all();
         $planos_futvolei = Plano::where('categoria_id', '=', 1)->get();
         $planos_funcional = Plano::where('categoria_id', '=', 2)->get();
+
         return view('aluno.cadastro-aluno', ['planos' => $planos, 'funcionais' => $planos_funcional, 'futvolei' => $planos_futvolei]);
     }
 
     public function save(Request $request)
     {
-            $aluno = new Aluno();
-            $aluno->fill($request->all());
-            $aluno->data_expiracao = now();
-            $aluno->data_cadastro = now();
-            $aluno->save();
-            return redirect()->route('listagem-alunos', 'todos')->withStatus(__('Aluno cadastrado com sucesso.'));
-    }
+        $aluno = new Aluno();
+        $aluno->fill($request->all());
+        $aluno->data_expiracao = now();
+        $aluno->data_cadastro = now();
+        $aluno->save();
 
+        return redirect()->route('listagem-alunos', 'todos')->withStatus(__('Aluno cadastrado com sucesso.'));
+    }
 }
