cabe9c2623e5e3a2f096dfcdd06e2419526b13b5
saucesar <sau.cesarlima2013@gmail.com>
2020-07-08 23:53:08 -0300

    Implementados graficos de Alunos por Categoria, Renda por Categoria e Matriculas por Mês.

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -2,10 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Categoria;
+use App\Plano;
+use Illuminate\Support\Facades\DB;
+
 class RelatorioController extends Controller
 {
     public function index()
     {
-        return view('relatorio/index');
+        $categorias = json_encode(Categoria::all('tipo')->pluck('tipo')->toArray());
+        $alunosPorPlano = [];
+        $rendaPorCategoria = [];
+
+        foreach (Categoria::all() as $categoria) {
+            $planos = Plano::where('categoria_id', $categoria->id)->pluck('id')->toArray();
+            $alunosPorPlano[] = DB::table('aluno_plano')->whereIn('plano_id', $planos)->count();
+
+            $totalCategoria = 0;
+
+            foreach (Plano::all() as $plano) {
+                if ($plano->categoria_id == $categoria->id) {
+                    $quantAlunos = DB::table('aluno_plano')->where('plano_id', $plano->id)->count();
+                    $totalCategoria += ($plano->preco * $quantAlunos);
+                }
+            }
+
+            $rendaPorCategoria[] = $totalCategoria;
+            $totalCategoria = 0;
+        }
+
+        $matriculasPorMes = [];
+
+        for ($mes = 1; $mes <= 12; ++$mes) {
+            $matriculasPorMes[] = DB::table('aluno_plano')->whereYear('created_at', date('Y'))->whereMonth('created_at', $mes)->count();
+        }
+
+        $dados = [
+            'categorias' => $categorias,
+            'alunosPorPlano' => json_encode($alunosPorPlano),
+            'rendaPorCategoria' => json_encode($rendaPorCategoria),
+            'meses' => json_encode(['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ']),
+            'matriculasPorMes' => json_encode($matriculasPorMes),
+        ];
+
+        return view('relatorio/index', $dados);
     }
 }
--- resources/views/relatorio/index.blade.php
+++ resources/views/relatorio/index.blade.php
@@ -6,24 +6,52 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header card-header-primary">
-                        <h4 class="card-title ">Relatorios</h4>
-                    </div>
                     <div class="card-body">
-                        @if (session('status'))
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="alert alert-success">
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <i class="material-icons">close</i>
-                                    </button>
-                                    <span>{{ session('status') }}</span>
-                                </div>
+                        <div class="table-responsive">
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <button class="btn btn-outline-primary" type="button" data-toggle="collapse"
+                                    data-target="#collapseAlunosPorCategoria" aria-expanded="false">
+                                    Alunos Por Categoria
+                                </button>
+                                <button class="btn btn-outline-primary" type="button" data-toggle="collapse"
+                                    data-target="#collapseRendaPorCategoria" aria-expanded="false">
+                                    Renda Por Categoria
+                                </button>
+                                <button class="btn btn-outline-primary" type="button" data-toggle="collapse"
+                                    data-target="#collapseMatriculasPorMes" aria-expanded="false">
+                                    Matriculas por Mês
+                                </button>
                             </div>
                         </div>
-                        @endif
-                        <div class="table-responsive">
-                           
+                    </div>
+                </div>
+                <div class="collapse" id="collapseMatriculasPorMes">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <p>Matriculas por Mês em {{date('Y')}}</p>
+                                <canvas id="matriculasPorMesChart" width=".5em" height=".5em"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="collapse" id="collapseRendaPorCategoria">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <p>Renda por Categoria</p>
+                                <canvas id="rendaPorCategoriaChart" width=".5em" height=".5em"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="collapse show" id="collapseAlunosPorCategoria">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <p>Alunos por Categoria</p>
+                                <canvas id="alunosPorCategoriaChart" width=".5em" height=".5em"></canvas>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -32,3 +60,122 @@
     </div>
 </div>
 @endsection
+
+@push('js')
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+<script>
+var ctx = document.getElementById('alunosPorCategoriaChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: <?= $categorias; ?> ,
+        datasets : [{
+            data: <?= $alunosPorPlano; ?> ,
+            backgroundColor : [
+                'rgba(55, 255, 32, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderColor: [
+                'rgb(255, 255, 255)',
+                'rgb(255, 255, 255)',
+
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
+
+<script>
+var ctx = document.getElementById('rendaPorCategoriaChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: <?= $categorias; ?> ,
+        datasets : [{
+            label: 'Renda por Categoria',
+            data: <?= $rendaPorCategoria; ?> ,
+            backgroundColor : [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
+<script>
+var ctx = document.getElementById('matriculasPorMesChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: <?= $meses; ?>,
+        datasets : [{
+            label: 'Matriculas por Mês',
+            data: <?= $matriculasPorMes; ?> ,
+            backgroundColor : [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
+@endpush
