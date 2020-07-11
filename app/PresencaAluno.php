963922938fe288ecbbd403dee0c6288d31e69076
saucesar <sau.cesarlima2013@gmail.com>
2020-07-10 23:31:18 -0300

    Adicao de metodos para pegar plano e aluno de uma presenca

--- app/PresencaAluno.php
+++ app/PresencaAluno.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class PresencaAluno extends Model
 {
     protected $fillable = ['plano_id', 'aluno_id'];
+
+    public function plano()
+    {
+        return Plano::firstWhere('id', $this->plano_id);
+    }
+
+    public function aluno()
+    {
+        return Aluno::firstWhere('id', $this->aluno_id);
+    }
 }
