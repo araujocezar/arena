cb8b63c6d1fd86d66e719edb2bf81a18665926b4
saucesar <sau.cesarlima2013@gmail.com>
2020-07-13 21:02:47 -0300

    Quebra do metodo em partes para facilitar leitura.

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Aluno;
 use App\Categoria;
 use App\Plano;
 use Illuminate\Support\Facades\DB;
@@ -11,40 +12,49 @@ class RelatorioController extends Controller
     public function index()
     {
         $categorias = json_encode(Categoria::all('tipo')->pluck('tipo')->toArray());
-        $alunosPorPlano = [];
+        $alunosPorCategoria = [];
         $rendaPorCategoria = [];
 
         foreach (Categoria::all() as $categoria) {
-            $planos = Plano::where('categoria_id', $categoria->id)->pluck('id')->toArray();
-            $alunosPorPlano[] = DB::table('aluno_plano')->whereIn('plano_id', $planos)->count();
+            $idDosPlanos = Plano::where('categoria_id', $categoria->id)->pluck('id')->toArray();
+            $alunosPorCategoria[] = DB::table('aluno_plano')->whereIn('plano_id', $idDosPlanos)->count();
 
-            $totalCategoria = 0;
-
-            foreach (Plano::all() as $plano) {
-                if ($plano->categoria_id == $categoria->id) {
-                    $quantAlunos = DB::table('aluno_plano')->where('plano_id', $plano->id)->count();
-                    $totalCategoria += ($plano->preco * $quantAlunos);
-                }
-            }
-
-            $rendaPorCategoria[] = $totalCategoria;
-            $totalCategoria = 0;
-        }
-
-        $matriculasPorMes = [];
-
-        for ($mes = 1; $mes <= 12; ++$mes) {
-            $matriculasPorMes[] = DB::table('aluno_plano')->whereYear('created_at', date('Y'))->whereMonth('created_at', $mes)->count();
+            $rendaPorCategoria[] = $this->calcularRendaCategoria($categoria->id);
         }
 
         $dados = [
             'categorias' => $categorias,
-            'alunosPorPlano' => json_encode($alunosPorPlano),
+            'alunosPorCategoria' => json_encode($alunosPorCategoria),
             'rendaPorCategoria' => json_encode($rendaPorCategoria),
             'meses' => json_encode(['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ']),
-            'matriculasPorMes' => json_encode($matriculasPorMes),
+            'matriculasPorMes' => $this->matriculasPorMes(),
         ];
 
         return view('relatorio/index', $dados);
+    }
+
+    private function matriculasPorMes()
+    {
+        $matriculasPorMes = [];
+
+        for ($mes = 1; $mes <= 12; ++$mes) {
+            $matriculasPorMes[] = Aluno::whereYear('created_at', date('Y'))->whereMonth('created_at', $mes)->count();
+        }
+
+        return json_encode($matriculasPorMes);
+    }
+
+    private function calcularRendaCategoria($categoriaId)
+    {
+        $totalCategoria = 0;
+
+        $planosDaCategoria = Plano::where('categoria_id', $categoriaId)->get();
+            
+        foreach ($planosDaCategoria as $plano) {
+            $quantAlunos = DB::table('aluno_plano')->where('plano_id', $plano->id)->count();
+            $totalCategoria += ($plano->preco * $quantAlunos);
+        }
+
+        return $totalCategoria;
     }
 }
